4abc88c9cf7083af444d1f39a64d2b3b4528cf65
hzb-macmini <3218801575@qq.com>
2025-08-04 22:57:18 +0800

    兼容微信

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { redirect, permanentRedirect } from "next/navigation";
 
 export default async function Home() {
     const h = await headers(); // ✅
-    const userAgent = h.get("user-agent") || ""; // ✅ 获取请求头中的 user-agent
+    const userAgent = h.get("user-agent")?.toLocaleLowerCase() || ""; // ✅ 获取请求头中的 user-agent
     const userHost = h.get("host") || ""; // ✅ 获取请求头中的 host
     const isTX = /micromessenger/.test(userAgent); // ✅ 判断是否为腾讯
     if (userHost.includes("h2b.cn") && !isTX) {
