f250d7badeea3551a83ff3a8e625edf0bf9bb7fa
hzb-macmini <3218801575@qq.com>
2025-08-04 22:44:35 +0800

    修改微信中的适配

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+    const h = await headers(); // ✅
+    const userAgent = h.get("user-agent") || ""; // ✅ 获取请求头中的 user-agent
+    const userHost = h.get("host") || ""; // ✅ 获取请求头中的 host
+    if (userHost === "h2b.cn") {
+        redirect("https://hzb.gg");
+    } else if (/micromessenger/.test(userAgent)) {
+        redirect("https://h2b.cn");
+    }
+
     return (
         <div className="bg-[url('/bg/background1.webp')] bg-center bg-no-repeat bg-fixed bg-cover">
             <div className="bg-black/20 font-sans grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -17,7 +27,8 @@ export default function Home() {
                             <div className="des-title text-xl">
                                 <span className="font-pacifico">I love you more than anything !</span>
                                 <br />
-                                <span>我爱你，胜过一切。</span>
+                                {/* <span>我爱你，胜过一切。</span> */}
+                                <span>愿得一心人，白首不分离。</span>
                             </div>
                             <span className="icon-[fa-solid--quote-right] font-pacifico mt-auto"></span>
                         </div>
