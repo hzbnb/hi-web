f3e8d397c3e04b7fa63508583a3234282e085fd6
hzb-macmini <3218801575@qq.com>
2025-08-04 23:01:25 +0800

    修改测试页面

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,13 +3,13 @@ import { redirect, permanentRedirect } from "next/navigation";
 
 export default async function Home() {
     const h = await headers(); // ✅
-    const userAgent = h.get("user-agent") || ""; // ✅ 获取请求头中的 user-agent
+    const userAgent = h.get("user-agent")?.toLocaleLowerCase() || ""; // ✅ 获取请求头中的 user-agent
     const userHost = h.get("host") || ""; // ✅ 获取请求头中的 host
     const isTX = /micromessenger/.test(userAgent); // ✅ 判断是否为腾讯
     if (userHost.includes("h2b.cn") && !isTX) {
-        permanentRedirect("https://hzb.gg");
+        permanentRedirect("https://hzb.gg/test");
     } else if (isTX) {
-        redirect("https://h2b.cn");
+        redirect("https://h2b.cn/test");
     }
 
     return (
